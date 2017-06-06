e74bd9cc0fe1087af28c51e347f8d9e15c30296b
ebouther <ebouther@student.42.fr>
2017-06-06 03:17:15 +0200

    colleen

---
+++ Colleen/src/colleen.c
@@ -0,0 +1,3 @@
+/*bonjour*/#include<stdio.h>
+void funct(){char*a="/*bonjour*/#include<stdio.h>%cvoid funct(){char*a=%c%s%c;printf(a,10,34,a,34,10,10);}%cint main(){/*toto*/funct();return (0);}%c";printf(a,10,34,a,34,10,10);}
+int main(){/*toto*/funct();return (0);}
