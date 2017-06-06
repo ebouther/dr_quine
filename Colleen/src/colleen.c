aed07513b7f4178104edea595f45def3ae747a27
ebouther <ebouther@student.42.fr>
2017-06-06 17:36:21 +0200

    reformat comments

--- Colleen/src/colleen.c
+++ Colleen/src/colleen.c
@@ -1,3 +1,10 @@
-/*bonjour*/#include<stdio.h>
-void funct(){char*a="/*bonjour*/#include<stdio.h>%cvoid funct(){char*a=%c%s%c;printf(a,10,34,a,34,10,10);}%cint main(){/*toto*/funct();return (0);}%c";printf(a,10,34,a,34,10,10);}
-int main(){/*toto*/funct();return (0);}
+/*
+   bonjour
+*/
+#include<stdio.h>
+void funct(){char*a="/*%c   bonjour%c*/%c#include<stdio.h>%cvoid funct(){char*a=%c%s%c;printf(a,10,10,10,10,34,a,34,10,10,10,10,10,10);}%cint main(){%c/*%c   toto%c*/%cfunct();return (0);}%c";printf(a,10,10,10,10,34,a,34,10,10,10,10,10,10);}
+int main(){
+/*
+   toto
+*/
+funct();return (0);}
