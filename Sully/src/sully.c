8e283e8f187169130dbee670801f11f3fd52c652
ebouther <ebouther@student.42.fr>
2017-06-12 15:59:03 +0200

    sully ok

--- Sully/src/sully.c
+++ Sully/src/sully.c
@@ -11,9 +11,8 @@ bzero(filename, 11);
 strcat(filename, "Sully_X.c");
 filename[6] = '0' + i;
 int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
-char*str="#include <stdio.h>%c#include <fcntl.h>%c#include <string.h>%c#include <stdlib.h>%c#include <unistd.h>%cint main()%c{%cint i = %d;%cchar *filename = malloc(sizeof(11));%cbzero(filename, 11);%cstrcat(filename, %cSully_X.c%c);%cfilename[6] = '0' + i;%cint fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);%cchar*str=%c%s%c;%cdprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, i - 1, 10, 10, 10, 34, 34, 10, 10, 10, 34, str, 34, 10, 10, 34, 34, 34, 34, 10, 34, 34, 34, 34, 10, 10, 10);%cexecl(%c/usr/bin/gcc%c, %cgcc%c, filename, NULL);%cexecl(%c./a.out%c, %ca.out%c, NULL);%creturn (0);%c}%c";
-dprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, i - 1, 10, 10, 10, 34, 34, 10, 10, 10, 34, str, 34, 10, 10, 34, 34, 34, 34, 10, 34, 34, 34, 34, 10, 10, 10);
-execl("/usr/bin/gcc", "gcc", filename, NULL);
-execl("./a.out", "a.out", NULL);
+char*str="#include <stdio.h>%c#include <fcntl.h>%c#include <string.h>%c#include <stdlib.h>%c#include <unistd.h>%cint main()%c{%cint i = %d;%cchar *filename = malloc(sizeof(11));%cbzero(filename, 11);%cstrcat(filename, %cSully_X.c%c);%cfilename[6] = '0' + i;%cint fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);%cchar*str=%c%s%c;%cdprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, i - 1, 10, 10, 10, 34, 34, 10, 10, 10, 34, str, 34, 10, 10, 34, 34, 10, 10, 10);%cchar *exec;asprintf(&exec, %c/usr/bin/gcc %%s -o sully_%%d && ./sully_%%d%c, filename, i, i);if(i>0)system(exec);%creturn (0);%c}%c";
+dprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, i - 1, 10, 10, 10, 34, 34, 10, 10, 10, 34, str, 34, 10, 10, 34, 34, 10, 10, 10);
+char *exec;asprintf(&exec, "/usr/bin/gcc %s -o sully_%d && ./sully_%d", filename, i, i);if(i>0)system(exec);
 return (0);
 }
