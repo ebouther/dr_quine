3b21b66f7e2f0258375959fbd319fd276f095ada
ebouther <ebouther@student.42.fr>
2017-06-20 21:28:57 +0200

    improved Sully

--- Sully/src/sully.c
+++ Sully/src/sully.c
@@ -7,13 +7,12 @@ int main()
 {
 int i = 5;
 if(getenv("child"))i--;
-char *filename = malloc(sizeof(11));
-bzero(filename, 11);
-strcat(filename, "Sully_X.c");
-filename[6] = '0' + i;
+char *filename = NULL;
+asprintf(&filename, "Sully_%d.c", i);
 int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
-char*str="#include <stdio.h>%c#include <fcntl.h>%c#include <string.h>%c#include <stdlib.h>%c#include <unistd.h>%cint main()%c{%cint i = %d;%cif(getenv(%cchild%c))i--;%cchar *filename = malloc(sizeof(11));%cbzero(filename, 11);%cstrcat(filename, %cSully_X.c%c);%cfilename[6] = '0' + i;%cint fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);%cchar*str=%c%s%c;%cdprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, i, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, str, 34, 10, 10, 34, 34, 10, 10, 10);%cchar *exec;asprintf(&exec, %c/usr/bin/gcc %%s -o sully_%%d && env -i child=1 ./sully_%%d%c, filename, i, i);if(i>0)system(exec);%creturn (0);%c}%c";
-dprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, i, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, str, 34, 10, 10, 34, 34, 10, 10, 10);
-char *exec;asprintf(&exec, "/usr/bin/gcc %s -o sully_%d && env -i child=1 ./sully_%d", filename, i, i);if(i>0)system(exec);
+char*str="#include <stdio.h>%c#include <fcntl.h>%c#include <string.h>%c#include <stdlib.h>%c#include <unistd.h>%cint main()%c{%cint i = %d;%cif(getenv(%cchild%c))i--;%cchar *filename = NULL;%casprintf(&filename, %cSully_%%d.c%c, i);%cint fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);%cchar*str=%c%s%c;%cdprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, i, 10, 34, 34, 10, 10, 34, 34, 10, 10, 34, str, 34, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10);%cchar *exec;asprintf(&exec, %c/usr/bin/gcc %%s -o Sully_%%d%c, filename, i);system(exec);%casprintf(&exec, %cenv -i child=1 ./Sully_%%d%c, i);if(i>0)system(exec);%creturn (0);%c}%c";
+dprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, i, 10, 34, 34, 10, 10, 34, 34, 10, 10, 34, str, 34, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10);
+char *exec;asprintf(&exec, "/usr/bin/gcc %s -o Sully_%d", filename, i);system(exec);
+asprintf(&exec, "env -i child=1 ./Sully_%d", i);if(i>0)system(exec);
 return (0);
 }
